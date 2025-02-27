a524b78c14585c973c56f89fc3a20f5514aef5df
Hosh Sadiq <hoshsadiq@users.noreply.github.com>
2025-02-27 12:49:41 +0000

    Use embed and remove deprecated function

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"go/ast"
 	"go/printer"
 	"go/token"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -302,7 +301,7 @@ func writeOptionsFile(types []string, packageName string, node ast.Node, fset *t
 		if err != nil {
 			log.Fatal(fmt.Errorf("template execute failed: %s", err))
 		}
-		if err := ioutil.WriteFile(outputFileName, buf.Bytes(), 0644); err != nil {
+		if err := os.WriteFile(outputFileName, buf.Bytes(), 0644); err != nil {
 			log.Fatal(fmt.Errorf("write failed: %s", err))
 		}
 		cmd := exec.Command("gofmt", "-w", outputFileName)
--- template.go
+++ template.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	_ "embed"
 	"strings"
 	"text/template"
 )
+
+//go:embed render.gotmpl
+var codeTemplateText string
+
+// store code generating template in constant
+var codeTemplate = template.Must(template.New("code").Funcs(funcMap).Parse(codeTemplateText))
 
 var funcMap = template.FuncMap{
 	"ToPrivate": func(s string) string {
@@ -13,9 +20,3 @@ var funcMap = template.FuncMap{
 		return strings.ToUpper(s[:1]) + s[1:]
 	},
 }
-
-// store code generating template in constant
-//go:generate bash -c "echo -e '// generated code, DO NOT EDIT\npackage main\n\nconst codeTemplateText = `' > template_text.go"
-//go:generate bash -c "cat render.gotmpl >> template_text.go"
-//go:generate bash -c "echo '`' >> template_text.go"
-var codeTemplate = template.Must(template.New("code").Funcs(funcMap).Parse(codeTemplateText))
